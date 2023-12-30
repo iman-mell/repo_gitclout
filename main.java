47b904a4431193f05893c11bc9d28dffe2d96ec5
Seer MENSAH ASSIAKOLEY <98156140+MaashRees@users.noreply.github.com>
2023-12-30 22:11:46 +0100

    Update main.java

--- main.java
+++ main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args){
 
-        System.out.println("Classe Main");
+        System.out.println("Classe Main de seer hihihiiiii");
         System.out.println("Ajout d'une nouvelle ligne");
     }
 }
